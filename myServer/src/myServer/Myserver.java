b5e9f8075e73b863678df0b5ed05cbaad2926dbc
ssww22 <siwon2664@gmail.com>
2024-12-03 17:22:31 +0900

    241203 완성 테스트X

--- myServer/src/myServer/Myserver.java
+++ myServer/src/myServer/Myserver.java
@@ -7,40 +7,19 @@ import java.util.*;
 public class Myserver {
 	public static void main(String[] args) {
 		ServerSocket svSocket = null;
-		BufferedReader in = null;
-		PrintWriter out = null;
-		
 		try {
+			System.out.println("클라이언트 연결 대기 중");
 			svSocket = new ServerSocket(10007);
-			Socket socket = svSocket.accept();
-			//신규유저면 이름 등록 
-			//아니면 채팅시작
-			out = new PrintWriter(socket.getOutputStream(),true);
-			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			//이름이 있으면 중복이라고 알려주기
-			//없으면 스레드 생성 후 채팅 시작
-			while(true) {
-				out.write("이름을 입력하세요\n");
-				out.flush();
-				//이름
-				String name = in.readLine();
-				if(NameHash.containsValue(name) == false) {
-					break;
-				}else {
-					continue;
-				}
-			}
-			// 인사 보내기 
-			
-			out.println("완료");
-			out.println(NameHash.getName(socket)+"님 환영합니다\n");
-			while(true) {
+			//1. 클라이언트가 연결됨 동시에 새로운 스레드 생성
+			//2. 이름 등록 및 메시지 처리
+			//3.채팅시작
+			while(true) {//클라이언트가 접속하면 새로운 스레드 생성 
+				Socket socket = svSocket.accept();
 				SvThread svThread = new SvThread(socket);
 				svThread.start();
 			}
-			
 		}catch(IOException e){
-			
+			System.out.println(e.getMessage());
 		}finally {
 			try {
 				svSocket.close();
@@ -53,16 +32,10 @@ public class Myserver {
 	}
 }
 class  NameHash{
-	private String name;
-	private Socket socket;
-	private String getName;
 	private static Map<Socket, String> nameHash = new HashMap<>();
 
-	public NameHash() {
-	}
-	public void register(Socket socket, String name) {//해시맵에 이름등록
-		this.socket =socket;
-		this.name = name;
+	
+	public static void register(Socket socket, String name) {//해시맵에 이름등록
 		nameHash.put(socket, name);
 	}
 	
@@ -75,10 +48,11 @@ class  NameHash{
 			//클라이언트에게 이름이 중복이라고 보내는 기능 만들기
 			return true;
 		}else {System.out.println("사용 가능한 이름입니다.");
+		
 		return false;
 	}
 		}
-	public boolean containsKey(Socket socket) {//키 값이 있는지 확인
+	public static boolean containsKey(Socket socket) {//키 값이 있는지 확인
 		return nameHash.containsKey(socket);
 	}
 }
@@ -90,12 +64,30 @@ class SvThread extends Thread{
 	}
 	public void run() {
 		try {
+			//스레드가 시작되면 해야 할 일
+			//1. 이름 등록
+			//2. 채팅
 			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			PrintWriter out = new PrintWriter (socket.getOutputStream());
+			PrintWriter out = new PrintWriter (socket.getOutputStream(),true);
+			//이름을 등록함
+			while(true) {
+				out.println("이름을 입력하세요");
+				String name = in.readLine();
+				
+				if(NameHash.containsValue(name) == false) {//이름 등록이 완료되면
+					NameHash.register(socket, name);
+					out.println("완료");
+					break;
+				}else {//실패하면
+					continue;
+				}
+			}
 			//채팅을 위한 문자열 변수 선언
 			String clientName = NameHash.getName(socket);
+			String inputMessage;
+			//채팅 시작
 			while(true) {
-				String inputMessage = in.readLine();
+				inputMessage = in.readLine();
 				if(inputMessage == null) {
 					out.println(clientName+"님이 접속을 종료했습니다.");
 					break;
@@ -106,13 +98,13 @@ class SvThread extends Thread{
 			System.out.println(e.getMessage()); 
 		}finally {
 			try {
+				//이름 삭제
+				 NameHash.register(socket, null);
 				socket.close();
+				 System.out.println("클라이언트 연결 종료");
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
-
 		}
 	}
 	
