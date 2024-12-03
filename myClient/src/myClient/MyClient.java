b80d353ca71c5bed01660f1a4fecc41d1a6de0df
ssww22 <siwon2664@gmail.com>
2024-12-03 17:22:04 +0900

    241203완성, 테스트X

--- myClient/src/myClient/MyClient.java
+++ myClient/src/myClient/MyClient.java
@@ -1,28 +1,26 @@
 package myClient;
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class MyClient {
 	public static void main(String[] args) {
-		BufferedReader in = null;
-		BufferedWriter out = null;
 		Scanner scanner = new Scanner(System.in);
-		String ip = "";
-		int port = 0;
+		String ip = "localhost";
+		int port = 10007;
 		try {
 			Socket socket = new Socket(ip,port);
-			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+			System.out.println("연결이 완료되었습니다.");
+			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 			//이름을 등록하는 루프
 			String inputMessage;
 			while(true) {
 				inputMessage = in.readLine();
-				if(inputMessage == "완료") {
+				if(inputMessage.equals("완료")) {
 					break;
 				}
 				System.out.println("서버 : "+ inputMessage);
@@ -33,12 +31,22 @@ public class MyClient {
 				ClientThread clientThread = new ClientThread(socket);
 				clientThread.start();
 			}
-			
+			ClientThread clientThread = new ClientThread(socket);
+			clientThread.start();//수신 스레드 시작
+			while(true) {//메시지 송신을 위함
+				String userMessage = scanner.nextLine();
+				if("exit".equalsIgnoreCase(userMessage)) {
+					System.out.println("채팅을 종료합니다.");
+					break;
+				}
+				out.println(userMessage);
+				
+			}
 			
 		}catch(IOException e) {
 			System.out.print(e.getMessage());
 		}finally {
-			
+			scanner.close();
 		}
 	}
 }
@@ -53,18 +61,15 @@ class ClientThread extends Thread{
 		try {
 			//채팅을 위한 준비 
 			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-			Scanner scanner = new Scanner(System.in);
-			String inputMessage, sendMessage;
+
+			String inputMessage;
 			//메시지를 읽음
-			inputMessage = in.readLine();
-			while(inputMessage != null) {
-				sendMessage = scanner.nextLine();
-				out.write(sendMessage);
-				out.flush();
+			while((inputMessage = in.readLine()) != null) {
+				System.out.println(inputMessage);
 			}
 			
-		}catch(IOException e1){System.out.print(e1.getMessage());}finally {
+			
+		}catch(IOException e1){System.out.print(e1.getMessage()+"서버와의 연결이 끊겼습니다.");}finally {
 			
 		}
 		
